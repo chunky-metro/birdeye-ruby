7ed4249f3829cd39988b8aba5e3227c9ef5f11dc
rhiza <rhiza@chunky.city>
2024-01-27 12:47:03 -0600

    initial commit

---
+++ lib/birdeye/ruby.rb
@@ -0,0 +1,8 @@
+require "birdeye/ruby/version"
+
+module Birdeye
+  module Ruby
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/birdeye/ruby_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Birdeye::Ruby do
+  it "has a version number" do
+    expect(Birdeye::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
